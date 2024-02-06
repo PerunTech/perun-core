a1a6626836d8eb18ca85b0feddf040d8706fd33d
tomi.karikj <t.karikj@prtech.mk>
2024-02-06 09:36:35 +0100

    removed some unused images and configuration files

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,4 +1,1 @@
 export { labelBasePath, svConfig } from './config';
-export { menuConfig } from './menuConfig';
-export { rolesConfig } from './rolesConfig';
-export { moduleSteps, searchSteps, farmerSteps, applicationSteps } from './tourSteps';
