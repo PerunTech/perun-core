f3b703b811fb5ecd254c33c7cefb55d4750892b7
tomi.karikj <t.karikj@prtech.mk>
2024-02-06 09:23:12 +0100

    overall clean-up
    removed a lot of unused components
    
    removed the react-css-modules package
    
    replaced the deprecated json2csv package with the up-to-date @json2csv/plainjs one and updated the implementation appropriately
    
    added the process package for appropriate local development

--- frontend/elements/index.js
+++ frontend/elements/index.js
@@ -15,9 +15,6 @@ export { Dropdown } from './base/dropdown/Dropdown';
 export { default as DependencyDropdown } from './base/dropdown/DependencyDropdown';
 export { default as InputElement } from './base/InputElement/InputElement';
 
-/* conversation */
-export { default as WrapConversation } from './conversation/WrapConversation';
-
 /* form */
 export { default as FormManager } from './form/FormManager';
 
@@ -27,19 +24,6 @@ export { default as ExportableGrid } from './grid/ExportableGrid';
 
 /* navigator, required by menu */
 export { default as ReduxNavigator } from './navigator/ReduxNavigator';
-
-/* menu */
-export { default as MainContent } from './menu/MainContent';
-export { default as MainMenuTop } from './menu/MainMenuTop/MainMenuTop';
-export { default as RecordInfo } from './menu/RecordInfo/RecordInfo';
-export { default as RecordInfoClass } from './menu/RecordInfo/RecordInfoClass'
-export { default as SideMenu } from './menu/SideMenu/SideMenu';
-export { default as EditSelectedItem } from './menu/SideMenu/ReturnedComponents/EditSelectedItem';
-export { default as GridContent } from './menu/SideMenu/ReturnedComponents/GridContent';
-
-/* selector */
-export { default as HandleItemSelection } from './selector/HandleItemSelection';
-export { default as SelectedItem } from './selector/SelectedItem';
 
 /* These should be moved to client.js */
 export { default as Draggable, DraggableCore } from 'react-draggable';
