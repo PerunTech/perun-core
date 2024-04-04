0738b4a346bd54d5f21f41c02d43eabfe64d361e
tomi.karikj <t.karikj@prtech.mk>
2024-04-04 09:30:43 +0200

    changed the label codes & formatting

--- frontend/components/NotFound/NotFound.js
+++ frontend/components/NotFound/NotFound.js
@@ -1,22 +1,28 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { iconManager } from "../../assets/svg/svgHolder";
-import createHashHistory from 'history/createHashHistory';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { iconManager } from '../../assets/svg/svgHolder'
+import createHashHistory from 'history/createHashHistory'
 
 const history = createHashHistory()
 const NotFound = (_props, context) => {
-    return (
-        <div className="not-found-container">
-            <div className="not-found-content">{iconManager.getIcon('notFound')}</div>
-            <p className="not-found-title">{context.intl.formatMessage({ id: 'perun.admin_console.not_found_title', defaultMessage: 'perun.admin_console.not_found_title' })}</p>
-            <p className="not-found-text">{context.intl.formatMessage({ id: 'perun.admin_console.not_found_text', defaultMessage: 'perun.admin_console.not_found_text' })}</p>
-            <p onClick={() => history.push('/')} className='not-found-btn'>{context.intl.formatMessage({ id: 'perun.admin_console.not_found_btn', defaultMessage: 'perun.admin_console.not_found_btn' })} </p>
-        </div>
-    )
+  return (
+    <div className='not-found-container'>
+      <div className='not-found-content'>{iconManager.getIcon('notFound')}</div>
+      <p className='not-found-title'>
+        {context.intl.formatMessage({ id: 'perun.main.not_found_title', defaultMessage: 'perun.main.not_found_title' })}
+      </p>
+      <p className='not-found-text'>
+        {context.intl.formatMessage({ id: 'perun.main.not_found_text', defaultMessage: 'perun.main.not_found_text' })}
+      </p>
+      <p onClick={() => history.push('/')} className='not-found-btn'>
+        {context.intl.formatMessage({ id: 'perun.main.not_found_btn', defaultMessage: 'perun.main.not_found_btn' })}
+      </p>
+    </div>
+  )
 }
 
 NotFound.contextTypes = {
-    intl: PropTypes.object.isRequired
+  intl: PropTypes.object.isRequired
 }
 
 export default NotFound
