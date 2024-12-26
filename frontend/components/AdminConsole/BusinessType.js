8e1b580b710a0e7d32dcd0df1b9798207b39cb9b
tomi.karikj <t.karikj@prtech.mk>
2024-12-26 15:13:58 +0100

    get the BUSINESS_OBJECT_TYPE_NAME system param to find out which table name should be used

--- frontend/components/AdminConsole/BusinessType.js
+++ frontend/components/AdminConsole/BusinessType.js
@@ -1,37 +1,64 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { ComponentManager, ExportableGrid, GenericForm, GridManager, axios } from '../../client'
-import { alertUser, ReactBootstrap } from '../../elements';
-const { useState, useEffect } = React;
-const { Modal } = ReactBootstrap;
-const tableName = 'HOLDING_TYPE';
-const gridId = `${tableName}_GRID`;
+import { ComponentManager, ExportableGrid, GenericForm, Loading, GridManager, axios } from '../../client'
+import { alertUser, ReactBootstrap } from '../../elements'
+const { useState, useEffect } = React
+const { Modal } = ReactBootstrap
 
 const BusinessType = (props, context) => {
-  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const [canRender, setCanRender] = useState(false)
+  const [businessObjectTypeName, setBusinessObjectTypeName] = useState(undefined)
+  const [gridId, setGridId] = useState(undefined)
+  const [show, setShow] = useState(false)
   const [objectId, setObjectId] = useState(undefined)
 
   useEffect(() => {
+    // Get the name of the table
+    getBusinessObjectTypeName()
+  }, [])
+
+  useEffect(() => {
     return () => {
-      ComponentManager.cleanComponentReducerState(gridId);
+      ComponentManager.cleanComponentReducerState(gridId)
     }
-  }, []);
+  }, [gridId])
+
+  const getBusinessObjectTypeName = () => {
+    setLoading(true)
+    const url = `${window.server}/WsConf/params/get/sys/BUSINESS_OBJECT_TYPE_NAME`
+    axios.get(url).then(res => {
+      setLoading(false)
+      if (res?.data?.VALUE) {
+        const objectTypeName = res.data.VALUE
+        setBusinessObjectTypeName(objectTypeName)
+        setGridId(`${objectTypeName}_GRID`)
+        setCanRender(true)
+      }
+    }).catch(err => {
+      setLoading(false)
+      console.error(err)
+      const title = err.response?.data?.title || err
+      const msg = err.response?.data?.message || ''
+      alertUser(true, 'error', title, msg)
+    })
+  }
 
   const handleRowClick = (_id, _rowIdx, row) => {
-    setObjectId(row[`${tableName}.OBJECT_ID`] || 0)
+    setObjectId(row[`${businessObjectTypeName}.OBJECT_ID`] || 0)
     setShow(true)
-  };
+  }
 
   const generateBusinessTypeGrid = () => {
-    const { svSession } = props;
+    const { svSession } = props
     return (
       <ExportableGrid
         gridType='READ_URL'
         key={gridId}
         id={gridId}
-        configTableName={`/ReactElements/getTableFieldList/${svSession}/${tableName}`}
-        dataTableName={`/ReactElements/getTableData/${props.svSession}/${tableName}/0`}
+        configTableName={`/ReactElements/getTableFieldList/${svSession}/${businessObjectTypeName}`}
+        dataTableName={`/ReactElements/getTableData/${props.svSession}/${businessObjectTypeName}/0`}
         onRowClickFunct={handleRowClick}
         refreshData={true}
         toggleCustomButton={true}
@@ -44,11 +71,11 @@ const BusinessType = (props, context) => {
         editContextFunc={handleRowClick}
       />
     )
-  };
+  }
 
   const saveRecord = (e) => {
-    const { svSession } = props;
-    const url = `${window.server}/ReactElements/createTableRecordFormData/${svSession}/${tableName}/0`;
+    const { svSession } = props
+    const url = `${window.server}/ReactElements/createTableRecordFormData/${svSession}/${businessObjectTypeName}/0`
     axios({
       method: 'post',
       data: encodeURIComponent(JSON.stringify(e.formData)),
@@ -60,40 +87,40 @@ const BusinessType = (props, context) => {
         const title = res.data.title || ''
         const msg = res.data.message || ''
         alertUser(true, resType, title, msg)
-        GridManager.reloadGridData(gridId);
-        setShow(false);
+        GridManager.reloadGridData(gridId)
+        setShow(false)
       }
     }).catch(err => {
       console.error(err)
       const title = err.response?.data?.title || err
       const msg = err.response?.data?.message || ''
-      alertUser(true, 'error', title, msg, () => ComponentManager.setStateForComponent(`${tableName}_FORM`, null, {
+      alertUser(true, 'error', title, msg, () => ComponentManager.setStateForComponent(`${businessObjectTypeName}_FORM`, null, {
         saveExecuted: false,
-      }));
-    });
-  };
+      }))
+    })
+  }
 
   const generateBusinessTypeForm = (objectId) => {
-    const { svSession } = props;
+    const { svSession } = props
     return (
       <GenericForm
         params={'READ_URL'}
-        key={`${tableName}_FORM`}
-        id={`${tableName}_FORM`}
-        method={`/ReactElements/getTableJSONSchema/${svSession}/${tableName}`}
-        uiSchemaConfigMethod={`/ReactElements/getTableUISchema/${svSession}/${tableName}`}
-        tableFormDataMethod={`/ReactElements/getTableFormData/${svSession}/${objectId}/${tableName}`}
+        key={`${businessObjectTypeName}_FORM`}
+        id={`${businessObjectTypeName}_FORM`}
+        method={`/ReactElements/getTableJSONSchema/${svSession}/${businessObjectTypeName}`}
+        uiSchemaConfigMethod={`/ReactElements/getTableUISchema/${svSession}/${businessObjectTypeName}`}
+        tableFormDataMethod={`/ReactElements/getTableFormData/${svSession}/${objectId}/${businessObjectTypeName}`}
         addSaveFunction={(e) => saveRecord(e)}
         hideBtns={objectId === 0 ? 'closeAndDelete' : 'close'}
         addDeleteFunction={deleteFunc}
         className={'admin-settings-forms'}
       />
     )
-  };
+  }
 
   const deleteFunc = (_id, _action, _session, formData) => {
-    const { svSession } = props;
-    const url = `${window.server}/ReactElements/deleteObject/${svSession}`;
+    const { svSession } = props
+    const url = `${window.server}/ReactElements/deleteObject/${svSession}`
     axios({
       method: 'post',
       data: encodeURIComponent(formData[4]['PARAM_VALUE']),
@@ -101,31 +128,34 @@ const BusinessType = (props, context) => {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     }).then((res) => {
       if (res.data.type === 'SUCCESS') {
-        alertUser(true, 'success', res.data.title, res.data.message);
-        setShow(false);
-        ComponentManager.setStateForComponent(`${tableName}_FORM`, null, {
+        alertUser(true, 'success', res.data.title, res.data.message)
+        setShow(false)
+        ComponentManager.setStateForComponent(`${businessObjectTypeName}_FORM`, null, {
           deleteExecuted: false,
-        });
+        })
         ComponentManager.setStateForComponent(gridId, null, {
           rowClicked: undefined,
-        });
-        GridManager.reloadGridData(gridId);
+        })
+        GridManager.reloadGridData(gridId)
       }
     }).catch(err => {
       console.error(err)
       const title = err.response?.data?.title || err
       const msg = err.response?.data?.message || ''
-      alertUser(true, 'error', title, msg, () => ComponentManager.setStateForComponent(`${tableName}_FORM`, null, {
+      alertUser(true, 'error', title, msg, () => ComponentManager.setStateForComponent(`${businessObjectTypeName}_FORM`, null, {
         deleteExecuted: false,
-      }));
-    });
-  };
+      }))
+    })
+  }
 
   return (
     <>
-      <div className='admin-console-grid-container'>
-        {generateBusinessTypeGrid()}
-      </div>
+      {loading && <Loading />}
+      {canRender && (
+        <div className='admin-console-grid-container'>
+          {generateBusinessTypeGrid()}
+        </div>
+      )}
       {show && (
         <Modal className='admin-console-unit-modal' show={show} onHide={() => setShow(false)}>
           <Modal.Header className='admin-console-unit-modal-header' closeButton>
@@ -138,15 +168,15 @@ const BusinessType = (props, context) => {
         </Modal>
       )}
     </>
-  );
-};
+  )
+}
 
 const mapStateToProps = (state) => ({
   svSession: state.security.svSession,
-});
+})
 
 BusinessType.contextTypes = {
   intl: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps)(BusinessType);
+export default connect(mapStateToProps)(BusinessType)
