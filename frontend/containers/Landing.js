d10e2e3f3bfc60f9d133f4d60eebc29ccb0faf03
tomi.karikj <t.karikj@prtech.mk>
2024-12-27 15:55:41 +0100

    converted the Landing component from a class to a functional one

--- frontend/containers/Landing.js
+++ frontend/containers/Landing.js
@@ -1,26 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import Logon from 'components/Logon/Logon'
 
-class Landing extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      component: null
-    }
-  }
+const Landing = () => {
+  useEffect(() => {
+    localStorage.clear()
+  }, [])
 
-  componentDidMount() {
-    localStorage.clear();
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Route path='/home/:logonComp' render={props => <Logon {...props} />} />
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <Route path='/home/:logonComp' render={props => <Logon {...props} />} />
+    </React.Fragment>
+  )
 }
 
 export default Landing
