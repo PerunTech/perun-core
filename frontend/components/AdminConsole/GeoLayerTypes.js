ec588472cccded739e8048f9dee2975ab457e3c3
tomi.karikj <t.karikj@prtech.mk>
2025-02-05 13:03:34 +0100

    reworked the component for geo layer types

--- frontend/components/AdminConsole/GeoLayerTypes.js
+++ frontend/components/AdminConsole/GeoLayerTypes.js
@@ -1,156 +1,151 @@
 import React from 'react'
-import { ComponentManager, ExportableGrid, GridManager, PropTypes, axios } from '../../client'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import Modal from '../Modal/Modal'
-import GenericForm from '../../elements/form/GenericForm'
-import { alertUser } from '../../elements'
+import { ComponentManager, ExportableGrid, GenericForm, Loading, GridManager, axios } from '../../client'
+import { alertUser, ReactBootstrap } from '../../elements'
+const { useReducer, useEffect } = React
+const { Modal } = ReactBootstrap
 
-const tableName = 'GEO_LAYER_TYPE'
-const formId = tableName + '_FORM'
-const gridId = tableName + '_GRID'
+const GeoLayerTypes = (props, context) => {
+  const initialState = { tableName: 'GEO_LAYER_TYPE', loading: false, gridId: undefined, show: false, objectId: 0 }
+  const reducer = (currState, update) => ({ ...currState, ...update })
+  const [{ tableName, loading, gridId, show, objectId }, setState] = useReducer(reducer, initialState)
 
-class GeoLayerTypes extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      grid: undefined,
-      geoLayerTypeFormModal: undefined,
+  useEffect(() => {
+    return () => {
+      ComponentManager.cleanComponentReducerState(gridId)
+    }
+  }, [gridId])
+
+  const handleRowClick = (_id, _rowIdx, row) => {
+    setState({ objectId: row[`${tableName}.OBJECT_ID`] || 0, show: true })
+  }
+
+  const generateGeoLayerTypeGrid = () => {
+    const { svSession } = props
+    return (
+      <ExportableGrid
+        gridType='READ_URL'
+        key={gridId}
+        id={gridId}
+        configTableName={`/ReactElements/getTableFieldList/${svSession}/${tableName}`}
+        dataTableName={`/ReactElements/getTableData/${props.svSession}/${tableName}/0`}
+        onRowClickFunct={handleRowClick}
+        refreshData={true}
+        toggleCustomButton={true}
+        customButton={() => {
+          setState({ show: true, objectId: 0 })
+        }}
+        customButtonLabel={context.intl.formatMessage({ id: 'perun.admin_console.add', defaultMessage: 'perun.admin_console.add' })}
+        heightRatio={0.75}
+        editContextFunc={handleRowClick}
+      />
+    )
+  }
+
+  const saveRecord = (e) => {
+    const { svSession } = props
+    const onConfirm = () => ComponentManager.setStateForComponent(`${tableName}_FORM`, null, { saveExecuted: false })
+    const formData = ComponentManager.getStateForComponent(`${tableName}_FORM`, 'formTableData')
+    const isEmpty = Object.values(formData).every(v => v === null || v === undefined)
+    if (!formData || isEmpty) {
+      const label = context.intl.formatMessage({ id: 'perun.admin_console.input_data_error', defaultMessage: 'perun.admin_console.input_data_error' })
+      alertUser(true, 'info', label, '', onConfirm)
+    } else {
+      const url = `${window.server}/ReactElements/createTableRecordFormData/${svSession}/${tableName}/0`
+      axios({
+        method: 'post',
+        data: encodeURIComponent(JSON.stringify(e.formData)),
+        url,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      }).then((res) => {
+        if (res.data) {
+          alertUserResponse(res.data)
+          GridManager.reloadGridData(gridId)
+          setState({ show: false })
+        }
+      }).catch(err => {
+        console.error(err)
+        const title = err.response?.data?.title || err
+        const msg = err.response?.data?.message || ''
+        alertUser(true, 'error', title, msg, onConfirm)
+      })
     }
   }
 
-  componentDidMount() {
-    this.getInitGrid()
-  }
-
-  componentWillUnmount() {
-    this.setState({ grid: undefined })
-    ComponentManager.cleanComponentReducerState(gridId)
-  }
-
-  getInitGrid = () => {
-    const { session } = this.props
-    const grid = (
-      <ExportableGrid
-        gridType={'READ_URL'}
-        key={gridId}
-        id={gridId}
-        configTableName={`/ReactElements/getTableFieldList/${session}/${tableName}`}
-        dataTableName={`/ReactElements/getTableData/${session}/${tableName}/0`}
-        onRowClickFunct={this.onRowClick}
-        toggleCustomButton
-        customButton={this.showModal}
-        refreshData={true}
-        heightRatio={0.75}
-        customButtonLabel={this.context.intl.formatMessage({ id: 'perun.admin_console.add', defaultMessage: 'perun.admin_console.add' })}
+  const generateGeoLayerTypeForm = (objectId) => {
+    const { svSession } = props
+    return (
+      <GenericForm
+        params={'READ_URL'}
+        key={`${tableName}_FORM`}
+        id={`${tableName}_FORM`}
+        method={`/ReactElements/getTableJSONSchema/${svSession}/${tableName}`}
+        uiSchemaConfigMethod={`/ReactElements/getTableUISchema/${svSession}/${tableName}`}
+        tableFormDataMethod={`/ReactElements/getTableFormData/${svSession}/${objectId}/${tableName}`}
+        addSaveFunction={(e) => saveRecord(e)}
+        hideBtns={objectId === 0 ? 'closeAndDelete' : 'close'}
+        addDeleteFunction={deleteFunc}
+        className={'admin-settings-forms'}
       />
     )
-
-    ComponentManager.setStateForComponent(gridId, null, { onRowClickFunct: this.onRowClick, customButton: this.showModal })
-    this.setState({ grid })
   }
 
-  onRowClick = (_gridId, _rowId, row) => {
-    const selectedObjId = row[`${tableName}.OBJECT_ID`]
-    this.showModal(selectedObjId)
-  }
-
-  saveGeoLayerType = (formData, url) => {
-    axios({ method: 'post', data: encodeURIComponent(JSON.stringify(formData)), url, headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).then(res => {
-      if (res.data) {
-        const resType = res.data.type.toLowerCase()
-        alertUser(true, resType, res.data.title)
-        if (resType === 'success') {
-          this.closeModal()
-          GridManager.reloadGridData(gridId)
-        }
+  const deleteFunc = (_id, _action, _session, formData) => {
+    const { svSession } = props
+    const url = `${window.server}/ReactElements/deleteObject/${svSession}`
+    axios({
+      method: 'post',
+      data: encodeURIComponent(formData[4]['PARAM_VALUE']),
+      url: url,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    }).then((res) => {
+      if (res.data.type === 'SUCCESS') {
+        alertUser(true, 'success', res.data.title, res.data.message)
+        setState({ show: false })
+        ComponentManager.setStateForComponent(`${tableName}_FORM`, null, {
+          deleteExecuted: false,
+        })
+        ComponentManager.setStateForComponent(gridId, null, {
+          rowClicked: undefined,
+        })
+        GridManager.reloadGridData(gridId)
       }
     }).catch(err => {
       console.error(err)
-      alertUser(true, 'error', err, '', () => ComponentManager.setStateForComponent(formId, null, { saveExecuted: false }))
+      const title = err.response?.data?.title || err
+      const msg = err.response?.data?.message || ''
+      alertUser(true, 'error', title, msg, () => ComponentManager.setStateForComponent(`${tableName}_FORM`, null, {
+        deleteExecuted: false,
+      }))
     })
   }
 
-  deleteGeoLayerType = (_id, _method, _session, params) => {
-    const { session } = this.props
-    const formData = params[4].PARAM_VALUE
-    const data = encodeURIComponent(JSON.stringify(JSON.parse(formData)))
-    const url = `${window.server}/ReactElements/deleteObject/${session}`
-    axios({ method: 'post', data, url, headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).then(res => {
-      if (res.data) {
-        alertUser(true, res.data.type.toLowerCase(), res.data.message)
-        GridManager.reloadGridData(gridId)
-        this.closeModal()
-      }
-    }).catch(err => {
-      alertUser(true, err.data.type.toLowerCase(), err.data.message)
-      this.closeModal()
-    })
-  }
-
-  showModal = (objectId) => {
-    const { session } = this.props
-    const geoLayerTypeObjId = objectId || 0
-    const modalTitle = objectId ? this.context.intl.formatMessage({ id: 'perun.admin_console.geo_layer_type', defaultMessage: 'perun.admin_console.geo_layer_type' })
-      : this.context.intl.formatMessage({ id: 'perun.admin_console.add_geo_layer_type', defaultMessage: 'perun.admin_console.add_geo_layer_type' })
-    const hideBtns = objectId ? 'close' : 'closeAndDelete'
-    const addDeleteFunction = objectId ? this.deleteGeoLayerType : null
-
-    const addSaveFunction = () => {
-      const formData = ComponentManager.getStateForComponent(formId, 'formTableData')
-      const isEmpty = Object.values(formData).every(v => v === null || v === undefined)
-      if (!formData || isEmpty) {
-        const label = this.context.intl.formatMessage({ id: 'perun.admin_console.input_data_error', defaultMessage: 'perun.admin_console.input_data_error' })
-        alertUser(true, 'info', label, '', () => ComponentManager.setStateForComponent(formId, null, { saveExecuted: false }))
-      } else {
-        const url = `${window.server}/ReactElements/createTableRecordFormData/${session}/GEO_LAYER_TYPE/0`
-        this.saveGeoLayerType(formData, url)
-      }
-    }
-
-    const form = (
-      <GenericForm
-        params={'READ_URL'}
-        key={formId}
-        id={formId}
-        method={`/ReactElements/getTableJSONSchema/${session}/${tableName}`}
-        uiSchemaConfigMethod={`/ReactElements/getTableUISchema/${session}/${tableName}`}
-        tableFormDataMethod={`/ReactElements/getTableFormData/${session}/${geoLayerTypeObjId}/${tableName}`}
-        hideBtns={hideBtns}
-        addSaveFunction={addSaveFunction}
-        addDeleteFunction={addDeleteFunction}
-      />
-    )
-
-    const geoLayerTypeFormModal = (
-      <Modal customClassBtnModal='customClassBtnModal'
-        closeModal={this.closeModal}
-        closeAction={this.closeModal}
-        modalContent={form}
-        modalTitle={modalTitle}
-        nameCloseBtn={this.context.intl.formatMessage({ id: 'perun.adminConsole.close', defaultMessage: 'perun.adminConsole.close' })}
-      />
-    )
-
-    this.setState({ geoLayerTypeFormModal })
-  }
-
-  closeModal = () => {
-    this.setState({ geoLayerTypeFormModal: undefined })
-  }
-
-  render() {
-    const { grid, geoLayerTypeFormModal } = this.state
-    return (
-      <React.Fragment>
-        <div className='admin-console-grid-container'>{grid}</div>
-        {geoLayerTypeFormModal}
-      </React.Fragment>
-    )
-  }
+  return (
+    <>
+      {loading && <Loading />}
+      <div className='admin-console-grid-container'>
+        {generateGeoLayerTypeGrid()}
+      </div>
+      {show && (
+        <Modal className='admin-console-unit-modal' show={show} onHide={() => setState({ show: false })}>
+          <Modal.Header className='admin-console-unit-modal-header' closeButton>
+            <Modal.Title>
+              {context.intl.formatMessage({ id: 'perun.admin_console.geo_layer_type', defaultMessage: 'perun.admin_console.geo_layer_type' })}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body className='admin-console-unit-modal-body'>
+            {generateGeoLayerTypeForm(objectId)}
+          </Modal.Body>
+          <Modal.Footer className='admin-console-unit-modal-footer'></Modal.Footer>
+        </Modal>
+      )}
+    </>
+  )
 }
 
-const mapStateToProps = state => ({
-  session: state.security.svSession
+const mapStateToProps = (state) => ({
+  svSession: state.security.svSession,
 })
 
 GeoLayerTypes.contextTypes = {
