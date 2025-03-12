e651374938e4d84411ec0a8caaf94b82fe90ca33
tomi.karikj <t.karikj@prtech.mk>
2025-03-12 11:22:56 +0100

    implemented the new alert and cleaned-up everything

--- frontend/components/AdminConsole/CodeListComp/CodeListEditor.js
+++ frontend/components/AdminConsole/CodeListComp/CodeListEditor.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from "react";
-import {
-    ComponentManager,
-    ExportableGrid,
-    GridManager,
-    Loading,
-    PropTypes
-} from "../../../client";
+import { ComponentManager, ExportableGrid, GridManager, Loading, PropTypes } from "../../../client";
 import { connect } from "react-redux";
-import { alertUser, ReactBootstrap } from "../../../elements";
+import { alertUserResponse, ReactBootstrap } from "../../../elements";
 const { Modal } = ReactBootstrap;
 import axios from "axios";
 import CodeListFormSchema from "./CodeListFormSchema";
@@ -32,28 +26,28 @@ const CodeListEditor = (props, context) => {
     }, [])
 
     const generateInnerGrid = (data, parentC) => {
+        const { svSession } = props
         ComponentManager.cleanComponentReducerState(prevId);
         const gridId = 'CODES_GRID_' + Math.floor(Math.random() * 999999).toString(36)
         prevId = gridId
         if (!data) {
             data = 0
         }
-        const { svSession } = props
-
-        let grid = (<ExportableGrid
-            gridType={"READ_URL"}
-            key={gridId}
-            id={gridId}
-            configTableName={`/ReactElements/getTableFieldList/${svSession}/SVAROG_CODES`}
-            dataTableName={`/ReactElements/getObjectsByParentId/${svSession}/${data}/SVAROG_CODES/10000`}
-            defaultHeight={false}
-            heightRatio={0.75}
-            refreshData={true}
-            onRowClickFunct={handleRowClick}
-            toggleCustomButton={true}
-            customButton={() => generateAddChildForm(parentC)}
-            customButtonLabel={context.intl.formatMessage({ id: 'perun.admin_console.add_code_list_editor', defaultMessage: 'perun.admin_console.add_code_list_editor' })}
-        />
+        const grid = (
+            <ExportableGrid
+                gridType={"READ_URL"}
+                key={gridId}
+                id={gridId}
+                configTableName={`/ReactElements/getTableFieldList/${svSession}/SVAROG_CODES`}
+                dataTableName={`/ReactElements/getObjectsByParentId/${svSession}/${data}/SVAROG_CODES/10000`}
+                defaultHeight={false}
+                heightRatio={0.75}
+                refreshData={true}
+                onRowClickFunct={handleRowClick}
+                toggleCustomButton={true}
+                customButton={() => generateAddChildForm(parentC)}
+                customButtonLabel={context.intl.formatMessage({ id: 'perun.admin_console.add_code_list_editor', defaultMessage: 'perun.admin_console.add_code_list_editor' })}
+            />
         );
         setInnerGrid(grid)
     }
@@ -68,7 +62,7 @@ const CodeListEditor = (props, context) => {
         generateEditForm(row['SVAROG_CODES.OBJECT_ID'])
         generateInnerGrid(row['SVAROG_CODES.OBJECT_ID'], row['SVAROG_CODES.CODE_VALUE'])
     }
-    //Edit label form
+
     const generateAddChildForm = (parentC, edit) => {
         if (!edit) {
             setShow(true)
@@ -94,35 +88,35 @@ const CodeListEditor = (props, context) => {
                 PARENT_CODE_VALUE: { "ui:readonly": true }
             }
         }
-        let form = <Form
-            key={'SVAROG_CODES_EDIT'}
-            uiSchema={uiSchema}
-            validator={validator}
-            schema={schema}
-            formData={formData}
-            className={`form-test label-form code-list-form ${edit && 'code-list-edit-form'}`}
-            onSubmit={(e) => {
-                submitCodeList(e, edit)
-            }}
-        >
-            <></>
-            <div className='admin-console-code-list-btn-holder'>
-                <div className='admin-console-label-form-btn-container'>
-                    <button className={`btn-success btn_save_form`} type="submit">
-                        {context.intl.formatMessage({ id: 'perun.generalLabel.save', defaultMessage: 'perun.generalLabel.save' })}
-                    </button>
-
+        const form = (
+            <Form
+                key={'SVAROG_CODES_EDIT'}
+                uiSchema={uiSchema}
+                validator={validator}
+                schema={schema}
+                formData={formData}
+                className={`form-test label-form code-list-form ${edit && 'code-list-edit-form'}`}
+                onSubmit={(e) => {
+                    submitCodeList(e, edit)
+                }}
+            >
+                <></>
+                <div className='admin-console-code-list-btn-holder'>
+                    <div className='admin-console-label-form-btn-container'>
+                        <button className={`btn-success btn_save_form`} type="submit">
+                            {context.intl.formatMessage({ id: 'perun.generalLabel.save', defaultMessage: 'perun.generalLabel.save' })}
+                        </button>
+                    </div>
+                    {edit && edit['PARENT_ID'] !== 0 && (
+                        <div className='admin-console-label-form-btn-container'>
+                            <button className='btn-success btn_save_form admin-console-code-list-remove-btn' type="button" onClick={() => deleteFunc(edit)}>
+                                {context.intl.formatMessage({ id: 'perun.generalLabel.delete', defaultMessage: 'perun.generalLabel.delete' })}
+                            </button>
+                        </div>
+                    )}
                 </div>
-                {edit && edit['PARENT_ID'] !== 0 && <div className='admin-console-label-form-btn-container'>
-                    <button className='btn-success btn_save_form admin-console-code-list-remove-btn' type="button" onClick={() => deleteFunc(edit)}>
-                        {context.intl.formatMessage({ id: 'perun.generalLabel.delete', defaultMessage: 'perun.generalLabel.delete' })}
-                    </button>
-                </div>}
-            </div>
-
-
-
-        </Form>
+            </Form>
+        )
         if (edit) {
             setEditForm(form)
         }
@@ -137,20 +131,18 @@ const CodeListEditor = (props, context) => {
             data: edit,
             url: restUrl,
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        }).then((response) => {
-            alertUser(true, response.data.type.toLowerCase(), response.data.title, response.data.message, () => {
-                backButton()
-            })
+        }).then((res) => {
             setLoading(false)
-            GridManager.reloadGridData(prevId)
+            if (res?.data) {
+                alertUserResponse({ response: res.data, onConfirm: () => backButton() })
+                GridManager.reloadGridData(prevId)
+            }
         }).catch(err => {
             console.error(err)
-            alertUser(true, 'error', err.data.title, err.data.message)
             setLoading(false)
+            alertUserResponse({ response: err.response?.data })
         })
     }
-    //save label
-
 
     const submitCodeList = (e, edit) => {
         setLoading(true)
@@ -170,20 +162,21 @@ const CodeListEditor = (props, context) => {
             data,
             url,
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        })
-            .then(function (response) {
-                alertUser(true, response.data.type?.toLowerCase() || 'info', response.data.title, response.data.message);
-                if (response.data.type === "SUCCESS") {
+        }).then((res) => {
+            setLoading(false)
+            if (res?.data) {
+                const resType = res.data?.type?.toLowerCase() || 'info'
+                alertUserResponse({ response: res.data })
+                if (resType === 'success') {
                     GridManager.reloadGridData(prevId);
                     setShow(false);
                 }
-                setLoading(false)
-            })
-            .catch(err => {
-                console.error(err)
-                alertUser(true, 'error', err.response.data.title || err, err.response.data.message || '')
-                setLoading(false)
-            })
+            }
+        }).catch(err => {
+            console.error(err)
+            setLoading(false)
+            alertUserResponse({ response: err.response?.data })
+        })
     };
 
     const backButton = () => {
