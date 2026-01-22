4e4675dcc69d6a559e2811bfa077f5224ae4dfa9
Kliment Kukoski <k.kukoski@prtech.mk>
2026-01-22 10:28:49 +0100

    removed unused menu components

--- frontend/components/MenuBuilder/MenuHolder.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import MenuFunctions from './MenuFunctions'
-
-export default function MenuHolder (props) {
-  return <MenuFunctions moduleNameProp={props.moduleNameProp} customJsonProp={props.customJsonProp} wsConfGetMenuProp={props.wsConfGetMenuProp}  customContRenderL={props.customContRenderL} customContRenderR={props.customContRenderR}/>
-}
