b115b4070229e8450f40aca84d6f614cad42e94a
tomi.karikj <t.karikj@prtech.mk>
2026-03-24 13:27:57 +0100

    revert for now

--- frontend/client.js
+++ frontend/client.js
@@ -1,73 +1,71 @@
-// Base libraries
+/* Base libraries */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import { Provider, connect } from 'react-redux';
+import { Provider, connect } from 'react-redux'; // these should be represented in /model and imported from there.
 import { persistStore } from 'redux-persist';
 import axios from 'axios'
 import md5 from 'md5'
 import Swal from 'sweetalert2'
-
-// Assets
+/* assets */
 import * as assets from './assets/index'; // eslint-disable-line
-
-// Routing & history
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // these should be represented in /routes and imported from there.
 import { generatePath } from 'react-router'
 import { createHashHistory } from 'history';
-
-// i18n
+/* i18n */
 import { addLocaleData } from 'react-intl';
 import { IntlProvider, updateIntl } from 'react-intl-redux';
-
-// Google Analytics
+/* Google Analytics */
 import ReactGA from 'react-ga';
-
-// Local modules
+/* Local modules */
 import * as redux from './model';
 import * as elements from './elements';
-import { Button, DependencyDropdown, Dropdown, InputElement, alertUserV2, alertUserResponse } from './elements'
 import { Configurator } from './loadConfiguration';
 import { router } from './routes/Router';
 import Routes from './routes/Routes';
 import { Loading } from 'components/ComponentsIndex';
 import { pluginManager } from './routes/PluginManager';
-import { ComponentManager } from './elements/ComponentManager'
-import Modal from './components/Modal/Modal.js'
+/* Utility functions */
+import * as utils from './functions/utils'
+import * as cookies from './functions/cookies'
 
-// Forms
+/* -------- */
+/* Assembly */
+/* -------- */
+export {
+  React, ReactDOM, PropTypes, Provider, connect, Link, generatePath, router, pluginManager, redux, elements, utils, Configurator,
+  axios, Loading, createHashHistory, md5, Swal
+};
+
+import { Button, DependencyDropdown, Dropdown, InputElement, alertUserV2, alertUserResponse } from './elements'
+import { Tooltip } from 'react-tooltip'
 import Form from '@rjsf/core';
 import validator from '@rjsf/validator-ajv8';
 import FormManager from './elements/form/FormManager'
 import GenericForm from './elements/form/GenericForm'
-
-// Grid
 import ContextMenuPopup from './elements/grid/ContextMenuPopup'
 import CustomGridToolbar from './elements/grid/CustomGridToolbar'
 import ExportableGrid from './elements/grid/ExportableGrid'
 import GenericGrid from './elements/grid/GenericGrid'
 import GridManager from './elements/grid/GridManager'
+import { ComponentManager } from './elements/ComponentManager'
+import Modal from './components/Modal/Modal.js'
 
-// UI
-import { Tooltip } from 'react-tooltip'
-
-// Utility functions
-import * as utils from './functions/utils'
-import * as cookies from './functions/cookies'
-
-// Exports
 export {
-  React, ReactDOM, PropTypes, Provider, connect, Link, generatePath, router, pluginManager, redux, elements, utils, Configurator,
-  axios, Loading, createHashHistory, md5, Swal,
   Form, Tooltip, Button, DependencyDropdown, Dropdown, InputElement, FormManager, GenericForm, ContextMenuPopup, CustomGridToolbar,
   ExportableGrid, GenericGrid, GridManager, ComponentManager, Modal, validator
 };
 
-// Axios interceptors
-
-// Check for arraybuffer response types
+/* check for arraybuffer response types */
 function validResponse(res) {
-  return !!res && res.responseType !== 'arraybuffer'
+  if (!res) {
+    return false
+  }
+  if (res.responseType === 'arraybuffer') {
+    return false
+  } else {
+    return true
+  }
 }
 
 axios.interceptors.request.use((config) => {
@@ -79,12 +77,14 @@ axios.interceptors.request.use((config) => {
 })
 
 axios.interceptors.response.use(
-  (res) => res,
+  (res) => {
+    return res;
+  },
   (error) => {
     if (error.response?.status) {
       switch (error.response.status) {
         case 302:
-          alertUserV2({ type: 'info', title: 'The server responded with a status code 302 Found' })
+          alertUserV2({ type: 'info', title: 'The server responsed with a status code 302 Found' })
           break;
         case 401:
           createHashHistory().push('/home/login')
@@ -100,27 +100,53 @@ axios.interceptors.response.use(
       }
     } else {
       const msg = error?.message || ''
-      if (msg.includes('401')) {
-        // If the accessed route is FR map, memorize it and write it in window.core
-        if (error.config.url.includes('/lpis/get/farmerAssets/undefined/')) {
-          if (window.location.href.includes('farm-registry/map')) {
-            window.core.memorizeFrMapRoute = window.location.href
+      switch (true) {
+        case msg.includes('401'):
+          /* if the accesed route is fr map memorize it and write it in window core */
+          if (error.config.url.includes('/lpis/get/farmerAssets/undefined/')) {
+            if (window.location.href.includes('farm-registry/map')) {
+              window.core.memorizeFrMapRoute = window.location.href
+            }
           }
-        }
-        createHashHistory().push('/home/login')
-      } else {
-        return Promise.reject(error);
+
+          createHashHistory().push('/home/login')
+          // location.reload();
+          break;
+        case msg.includes('500'):
+          return Promise.reject(error);
+        case msg.includes('502'):
+          return Promise.reject(error);
+        default:
+          return Promise.reject(error);
       }
     }
   }, { runWhen: validResponse }
 );
 
-// Init & configuration
+const App = () => (
+  <Provider store={redux.store}>
+    <IntlProvider>
+      <Routes />
+    </IntlProvider>
+  </Provider>
+)
+/* if user acces fr route memorize it */
+let memorizeFrMapRoute
+if (window.location.href.includes('farm-registry/map')) {
+  memorizeFrMapRoute = window.location.href
+  createHashHistory().push('/home/login')
+}
+
+window.core = { React, ReactDOM, Provider, connect, Link, router, pluginManager, redux, elements, Configurator, memorizeFrMapRoute }
+
+/* ---- */
+/* Init */
+/* ---- */
 const app = document.getElementById('app');
 let persistConfig = {}
 let whitelist = []
 let indexReducer = 0
-const whitelistRoot = [
+let whitelistRoot = [
   'checkForInvalidSession',
   'configurator',
   'globalRequestProcessor',
@@ -134,8 +160,8 @@ const whitelistRoot = [
   'businessLogicReducer'
 ]
 
-// Use the default locale defined in the assets project.
-// Falls back to English (en_US) if not set.
+// Use the default locale defined in the assets project
+// If the default locale doesn't exist, fallback to the english one
 const defaultLocale = cookies.getCookie('defaultLocale') || 'en_US'
 let localeData = ['en']
 if (utils.isJSON(cookies.getCookie('localeData'))) {
@@ -152,83 +178,12 @@ async function loadLocaleData(locale) {
   }
 }
 
-// Google Analytics
-if (process.env.GA_TRACKING_ID) {
-  ReactGA.initialize(process.env.GA_TRACKING_ID);
-  ReactGA.pageview('/');
-}
+/* call function on perun-core initialization */
+persistBundleReducers(whitelistRoot)
 
-// App component
-const App = () => (
-  <Provider store={redux.store}>
-    <IntlProvider>
-      <Routes />
-    </IntlProvider>
-  </Provider>
-)
+changeLanguageAndLocale(defaultLocale?.replace('_', '-'), defaultLocale)
 
-// If user accesses FR route, memorize it
-let memorizeFrMapRoute
-if (window.location.href.includes('farm-registry/map')) {
-  memorizeFrMapRoute = window.location.href
-  createHashHistory().push('/home/login')
-}
-
-window.core = { React, ReactDOM, Provider, connect, Link, router, pluginManager, redux, elements, Configurator, memorizeFrMapRoute }
-
-// Bootstrap & render
-let appBootstrapPromise
-function waitForAppBootstrap() {
-  if (!appBootstrapPromise) {
-    appBootstrapPromise = router.waitForPlugins().catch((error) => {
-      console.error('Plugin bootstrap failed:', error)
-    })
-  }
-  return appBootstrapPromise
-}
-
-function renderApp() {
-  waitForAppBootstrap().then(() => {
-    app && ReactDOM.render(<App />, app);
-  })
-}
-
-function getAdditionalLabels(allLabels, locale, language, additionalLabels) {
-  redux.dataToRedux((response) => {
-    redux.store.dispatch(updateIntl({ locale: locale, messages: { ...allLabels, ...response } }));
-    renderApp()
-  },
-    'security',
-    'temp',
-    'MAIN_LABELS',
-    language,
-    additionalLabels
-  );
-}
-
-export function changeLanguageAndLocale(locale, language) {
-  ReactDOM.render(<Loading />, app);
-  persistStore(redux.store, persistConfig, () => {
-    redux.dataToRedux((response) => {
-      document.cookie = `defaultLocale=${language};max-age=${(365 * 24 * 60 * 60)}`
-      redux.store.dispatch(updateIntl({ locale: locale, messages: response }));
-      const additionalLabels = cookies.getCookie('additionalLabels')
-      if (additionalLabels) {
-        getAdditionalLabels(response, locale, language, additionalLabels)
-      } else {
-        renderApp()
-      }
-    },
-      'security',
-      'temp',
-      'MAIN_LABELS',
-      language,
-      'perun'
-    );
-  })
-}
-
-// Exportable function to add persist reducers from bundle
+/* exportable function to add persist reducers from bundle */
 export function persistBundleReducers(listOfBundleReducers) {
   let res = whitelist.filter(item => listOfBundleReducers.includes(item));
   if (res.length > 0) {
@@ -236,7 +191,7 @@ export function persistBundleReducers(listOfBundleReducers) {
   } else {
     whitelist.push(...listOfBundleReducers)
 
-    // If reducers come from perun-core, skip the incrementation
+    /* if reducers come from perun-core, skip the incrementation */
     if (!listOfBundleReducers.includes('intl')) {
       indexReducer++
     }
@@ -257,5 +212,55 @@ export function persistBundleReducers(listOfBundleReducers) {
   }
 }
 
-// Initialize on perun-core load
-persistBundleReducers(whitelistRoot)
+ReactGA.initialize('UA-138995187-1');
+ReactGA.pageview('/');
+
+let appBootstrapPromise
+function waitForAppBootstrap() {
+  if (!appBootstrapPromise) {
+    appBootstrapPromise = router.waitForPlugins().catch((error) => {
+      console.error('Plugin bootstrap failed:', error)
+    })
+  }
+  return appBootstrapPromise
+}
+
+function getAdditionalLabels(allLabels, locale, language, additionalLabels) {
+  redux.dataToRedux((response) => {
+    redux.store.dispatch(updateIntl({ locale: locale, messages: { ...allLabels, ...response } }));
+    waitForAppBootstrap().then(() => {
+      app && ReactDOM.render(<App />, app);
+    })
+  },
+    'security',
+    'temp',
+    'MAIN_LABELS',
+    language,
+    additionalLabels
+  );
+}
+
+export function changeLanguageAndLocale(locale, language) {
+  ReactDOM.render(<Loading />, app);
+  persistStore(redux.store, persistConfig, () => {
+    redux.dataToRedux((response) => {
+      document.cookie = `defaultLocale=${language};max-age=${(365 * 24 * 60 * 60)}`
+      redux.store.dispatch(updateIntl({ locale: locale, messages: response }));
+      // Check if any additional labels should be fetched
+      const additionalLabels = cookies.getCookie('additionalLabels')
+      if (additionalLabels) {
+        getAdditionalLabels(response, locale, language, additionalLabels)
+      } else {
+        waitForAppBootstrap().then(() => {
+          app && ReactDOM.render(<App />, app);
+        })
+      }
+    },
+      'security',
+      'temp',
+      'MAIN_LABELS',
+      language,
+      'perun'
+    );
+  })
+}
