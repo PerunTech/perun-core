28aaa89bfdd64e4eca963c3c5256dc5732f81e6e
tomi.karikj <t.karikj@prtech.mk>
2026-05-20 13:04:46 +0200

    improve SystemConfLogs grid config and log preview
    - Fetch field list and append DT_INSERT column via SEARCH_GRID_DATA
    - Render NOTE_NAME as a bold label and NOTE_TEXT as a <pre> log viewer
    - Show a label when no note data is returned for a row

--- frontend/components/AdminConsole/SystemConfLogsWrapper.js
+++ frontend/components/AdminConsole/SystemConfLogsWrapper.js
@@ -1,22 +1,17 @@
-import React from 'react';
-import { ComponentManager } from '../../elements';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
-const { useState, useEffect } = React;
-const SvarogNotesWrapper = (props) => {
+import { ComponentManager } from '../../elements';
+
+const SvarogNotesWrapper = (props, context) => {
     const [shouldRender, setRender] = useState(false)
     useEffect(() => {
         changeField()
     }, [])
 
-
     const changeField = () => {
         const { formid } = props
-        const jsonSchema = ComponentManager.getStateForComponent(
-            formid,
-            "formData"
-        );
-
+        const jsonSchema = ComponentManager.getStateForComponent(formid, "formData");
         if (jsonSchema) {
             delete jsonSchema.properties['NOTE_TEXT'].format
             ComponentManager.setStateForComponent(formid, "formData", jsonSchema);
@@ -25,16 +20,27 @@ const SvarogNotesWrapper = (props) => {
         }
     }
 
-    return (
-        <>
-            {shouldRender && props.children}
-        </>
-    );
+    if (!shouldRender) return null
+
+    const { formid } = props
+    const formDataLoaded = ComponentManager.getStateForComponent(formid, 'formDataLoaded')
+    const formTableData = ComponentManager.getStateForComponent(formid, 'formTableData')
+
+    if (formDataLoaded === true && !formTableData) {
+        return (
+            <p className='conf-log-no-data'>
+                {context.intl.formatMessage({ id: 'perun.admin_console.no_log_to_show', defaultMessage: 'perun.admin_console.no_log_to_show' })}
+            </p>
+        )
+    }
+
+    return <>{props.children}</>
 };
 
 const mapStateToProps = (state) => ({
     svSession: state.security.svSession,
 });
+
 SvarogNotesWrapper.contextTypes = {
     intl: PropTypes.object.isRequired,
 };
